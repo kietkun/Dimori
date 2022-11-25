7d4e564874824c98c727056349809c4b33cc7636
kietkkun191 <kietnguyen.work@gmail.com>
2022-11-25 14:10:20 +0700

    commit

--- src/utils/contracts-config.js
+++ src/utils/contracts-config.js
@@ -1,5 +1,5 @@
 
-      export const contractAddress = "0xf415CFA919Db7E828396dddaF803D12DcF87F2f9"
-      export const ownerAddress = "0x3954698510b7de148d23dc3e7Fe6C4F72d8065E5"
+      export const contractAddress = "0x8108923719518e3e5419C9248bafb7F4b66Fb965"
+      export const ownerAddress = "0xC8d0C06ad0442d40d10F602796102CA543a02cf9"
       export const networkDeployedTo = "80001"
     

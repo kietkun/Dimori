74d99defa0aaf7d3446f5c3496f8e9b9e77adb6c
kietkkun191 <kietnguyen.work@gmail.com>
2022-12-08 22:30:48 +0700

    hash router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { AddRental, Home, Rentals, BookedSchedules, YourRentals } from './pages'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 
 function App() {
 
     return (
         <div>
-            <Router>
+            <HashRouter>
                 <Routes>
-                    <Route path="/#/" element={<Home />} />
-                    <Route path="/#/rentals" element={<Rentals />} />
-                    <Route path="/#/add-rental" element={<AddRental />} />
-                    <Route path="/#/booked-schedules" element={<BookedSchedules />} />
-                    <Route path="/#/your-rentals" element={<YourRentals />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/rentals" element={<Rentals />} />
+                    <Route path="/add-rental" element={<AddRental />} />
+                    <Route path="/booked-schedules" element={<BookedSchedules />} />
+                    <Route path="/your-rentals" element={<YourRentals />} />
                 </Routes>
-            </Router>
+            </HashRouter>
         </div>
     );
 }
